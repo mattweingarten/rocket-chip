e286e4013dbd83ff086e043fb2fec89e56e020ad
mattweingarten <matthew.weingarten@inf.ethz.ch>
2024-11-26 13:12:07 -0500

    Add libgloss to setup in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ git remote set-url origin git@github.com:mattweingarten/chipyard.git
 
 git checkout rocket_chip_cpi
 
-./build-setup.sh riscv-tools  -s 3 -s 4 -s 5 -s 6 -s 7 -s 8 -s 9
-
+./build-setup.sh riscv-tools -s 4 -s 5 -s 6 -s 7 -s 8 -s 9
 
 cd ../
